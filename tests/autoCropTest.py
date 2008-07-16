31a217a31f0b3fcda86b433e3eb0288a960624bc
Raj Kumar (IA-Books)* <rkumar@home.us.archive.org>
2008-07-16 00:34:33 +0000

    also show human crop/deskew.

--- tests/autoCropTest.py
+++ tests/autoCropTest.py
@@ -8,6 +8,7 @@ import commands
 import glob
 import os
 import re
+import xml.etree.ElementTree as ET
 
 files = glob.glob("*.jpg")
 assert len(files) > 0
@@ -16,19 +17,26 @@ outDir     = '/home/rkumar/public_html/autocrop/picturesquenewen00swee'
 proxyDir   = 'proxy'
 skewedDir  = 'skewed'
 croppedDir = 'cropped'
+humanDir   = 'human'
 
 assert not os.path.exists(outDir)
 assert not os.path.exists(outDir + '/' + proxyDir)
 assert not os.path.exists(outDir + '/' + skewedDir)
 assert not os.path.exists(outDir + '/' + croppedDir)
+assert not os.path.exists(outDir + '/' + humanDir)
+assert os.path.exists('scandata.xml');
 
 os.mkdir(outDir)
 os.mkdir(outDir + '/' + proxyDir)
 os.mkdir(outDir + '/' + skewedDir)
 os.mkdir(outDir + '/' + croppedDir)
+os.mkdir(outDir + '/' + humanDir)
 
 outHtml = outDir + '/skew.html'
 assert not os.path.exists(outHtml)
+
+scandata = ET.parse('scandata.xml').getroot()
+leafs = scandata.findall('.//page')
 
 f = open(outHtml, 'w')
 f.writelines(['<html>\n', '<head><title>skew test for %s</title></head>\n'%os.path.basename(os.getcwd()), '<body>\n', '<table border=2>\n']);
@@ -68,6 +76,22 @@ for file in sorted(files):
     retval = commands.getstatusoutput('cp ~/public_html/outcrop.jpg "%s/%s/%d.jpg"'%(outDir, croppedDir,leafNum))[0]
     assert (0 == retval)    
     
+    leaf=leafs[leafNum]
+    skew = float(leaf.findtext('skewAngle'))
+    #score = float(leaf.findtext('skewScore'))
+    skewAct = leaf.findtext('skewActive')
+    cropx = int(leaf.findtext('cropBox/x'))
+    cropy = int(leaf.findtext('cropBox/y'))
+    cropw = int(leaf.findtext('cropBox/w'))
+    croph = int(leaf.findtext('cropBox/h'))
+    if "false" == skewAct:
+        skew = 0.0
+    
+    #print "crop x,y,w,h = %d,%d %d,%d"%(cropx, cropy, cropw, croph)
+    #print '/home/rkumar/gnubook/tests/cropAndSkewProxy %s "%s/%s/%d.jpg" %d %f %d %d %d %d'%(file, outDir, humanDir,leafNum, rotateDir, skew, cropx, cropy, cropw, croph)
+    retval = commands.getstatusoutput('/home/rkumar/gnubook/tests/cropAndSkewProxy %s "%s/%s/%d.jpg" %d %f %d %d %d %d'%(file, outDir, humanDir,leafNum, rotateDir, skew, cropx/8, cropy/8, cropw/8, croph/8))[0]
+    assert (0 == retval)    
+
     f.write('<tr>\n')
     f.write('<td valign=top>'),
     f.write('<img src="%s/%d.jpg"/>'%(proxyDir,leafNum))
@@ -82,7 +106,12 @@ for file in sorted(files):
     f.write('<img src="%s/%d.jpg"/>'%(croppedDir,leafNum))
     f.write("<br>skewMode: %s"%(skewMode))
     f.write("<br>textSkew: %0.2f, textScore: %0.2f"%(textSkew, textScore))
+    f.write('</td>\n')
 
+    f.write('<td valign=top>'),
+    f.write("human crop and skew:<br>")
+    f.write('<img src="%s/%d.jpg"/>'%(humanDir,leafNum))
+    f.write("<br>Skew: %0.2f, crop x,y w,h = %d,%d %d,%d"%(skew, cropx, cropy, cropw, croph))
     f.write('</td>\n')
 
     f.write('</tr>\n')
