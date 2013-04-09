fc02497570708a60e7b86f2ba3fcdb70be71aa11
rajbot <raj@archive.org>
2013-04-09 00:29:17 +0000

    Update Makefile so `make test` now runs foldout tests as well. Update README with build and test instructions.

--- tests/processTestImages.py
+++ tests/processTestImages.py
@@ -2,7 +2,7 @@
 
 """
 This script preprocesses images from an IA Scribe bookscanner.
-For each image found in the testimg directory, it calculates 
+For each image found in the testimg directory, it calculates
 a cropbox and skew angle, and writes the results back into outdir.
 """
 
@@ -12,16 +12,20 @@ import re
 import sys
 import glob
 import datetime
+from os.path import join, exists
 
 testimg_dir = 'testimg'
 testrun_dir = 'testrun'
-today_dir   = testrun_dir + '/' + datetime.date.today().isoformat()
-out_dir     = today_dir + '/' + datetime.datetime.now().isoformat()
+today       = datetime.date.today().isoformat()
+now         = datetime.datetime.now().isoformat()
+today_dir   = join(testrun_dir, today)
+out_dir     = join(testrun_dir, today, now)
 autocrop    = '../autoCropScribe'
 
 proxy_dir   = 'proxy'
 box_dir     = 'box'
 cropped_dir = 'cropped'
+debug_img_dir = 'debug-images'
 
 assert os.path.exists(testimg_dir)
 assert not os.path.exists(out_dir)
@@ -32,18 +36,29 @@ assert len(files) > 0
 
 if not os.path.exists(today_dir):
     os.mkdir(today_dir)
-    
+
 os.mkdir(out_dir)
-os.mkdir(out_dir + '/' + proxy_dir)
-os.mkdir(out_dir + '/' + box_dir)
-os.mkdir(out_dir + '/' + cropped_dir)
+os.mkdir(join(out_dir, proxy_dir))
+os.mkdir(join(out_dir, box_dir))
+os.mkdir(join(out_dir, cropped_dir))
 
 latest_link = testrun_dir + '/latest'
-if os.path.exists(latest_link):
-    os.unlink(latest_link)
-os.symlink(out_dir, latest_link)    
+if os.path.lexists(latest_link):
+    ret = os.unlink(latest_link)
+os.symlink(join(today, now), latest_link)
 
 
+# move()
+#_________________________________________________________________________________________
+def move(src, dest):
+    try:
+        os.rename(src, dest)
+    except OSError:
+        sys.exit('Could not move debug image %s. Please make sure you compile with `make CXXFLAGS=-DWRITE_DEBUG_IMAGES`' % src)
+
+
+# __main__
+#_________________________________________________________________________________________
 print "Creating test output in " + out_dir
 
 out_html = out_dir + '/index.html'
@@ -53,16 +68,16 @@ f.writelines(['<html>\n', '<head><title>autocrop test for %s</title></head>\n'%d
 for file in sorted(files):
     print "Processing %s"%(file)
     base_file = os.path.basename(file)
-    
+
     m = re.search('(\d{4})\.(jpg|JPG)$', file)
     leafnum = int(m.group(1))
     print 'leafNum is %d' % leafnum
-    
+
     if (1 == (leafnum & 0x1)):
         rotate_direction = 1
     else:
         rotate_direction = -1
-    
+
     cmd = "../autoCropScribe %s %d" % (file, rotate_direction)
     print cmd
     retval,output = commands.getstatusoutput(cmd)
@@ -71,7 +86,7 @@ for file in sorted(files):
     m=re.search('skewMode: (\w+)', output)
     skewMode = m.group(1)
     print "skewMode is " + skewMode
-    
+
     m=re.search('angle: ([-.\d]+)', output)
     assert(None != m)
     textSkew = float(m.group(1))
@@ -79,23 +94,19 @@ for file in sorted(files):
     m=re.search('conf: ([-.\d]+)', output)
     assert(None != m)
     textScore = float(m.group(1))
-    
+
     m=re.search('bindingAngle: ([-.\d]+)', output)
     assert(None != m)
     bindingSkew = float(m.group(1))
 
     m=re.search('grayMode: ([\w-]+)', output)
     grayMode = m.group(1)
-    print "grayMode is " + grayMode    
-    
-    retval = commands.getstatusoutput('cp ./debug-images/out.jpg "%s/%s/%s"'%(out_dir, proxy_dir, base_file))[0]
-    assert (0 == retval)    
+    print "grayMode is " + grayMode
 
-    retval = commands.getstatusoutput('cp ./debug-images/outbox.jpg "%s/%s/%s"'%(out_dir, box_dir, base_file))[0]
-    assert (0 == retval)    
-
-    retval = commands.getstatusoutput('cp ./debug-images/outcrop.jpg "%s/%s/%s"'%(out_dir, cropped_dir, base_file))[0]
-    assert (0 == retval)    
+    #move debug images from debug_img_dir into outdir
+    move(join(debug_img_dir, 'out.jpg'),     join(out_dir, proxy_dir, base_file))
+    move(join(debug_img_dir, 'outbox.jpg'),  join(out_dir, box_dir, base_file))
+    move(join(debug_img_dir, 'outcrop.jpg'), join(out_dir, cropped_dir, base_file))
 
     f.write('<tr>\n')
     f.write('<td valign=top>'),
