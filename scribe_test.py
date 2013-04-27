7a4d6c1299fbfed9e314da4a577197ef854d1263
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-04-26 21:43:59 -0700

    added test case for addCropBox

--- scribe_test.py
+++ scribe_test.py
@@ -9,17 +9,25 @@
 from lxml import etree
 from StringIO import StringIO
 import unittest
-from processScribe import rmtag
+import processScribe as ps
 
 XML = {'rmtag': "<parent><child>1</child><child>2</child></parent>"}       
 
 class TestScribe(unittest.TestCase):
 
-    def test_rmtag(self):
+    def test_rmtag(self):        
         xmltree = etree.parse(StringIO(XML['rmtag']))
         assert(len(xmltree.findall("child")) == 2)
-        rmtag("child", xmltree.getroot())
+        ps.rmtag("child", xmltree.getroot())
         assert(not len(xmltree.findall("child")))
 
-
-
+    def test_addCropBox(self):
+        xmltree = etree.parse(StringIO(XML['rmtag']))
+        tag = "child"
+        parent = xmltree.getroot()
+        xywh = [1, 2, 3, 4]
+        ps.addCropBox(tag, parent, *xywh)
+        child = xmltree.getroot().findall("child")[0]
+        for tagname, val in zip(['x', 'y', 'w', 'h'], xywh):
+            assert(len(child.findall(tagname)))
+            assert(int(child.findall(tagname)[0].text) == val)
