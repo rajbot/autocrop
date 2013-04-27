904dbb00535ea9d14f10ee78b9b961366a14e2d2
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-04-26 22:24:26 -0700

    general refactor and test cases
    - updated document header info
    - simplified mean_var function (and renamed calc_mean_var to mean_var
    - started refactor of addCropBoxAutoDetect
    - removed # ___  style dividers and added space before and after = signs
    - factored parse_attrs out of pass1 algo
    - factored get_jpg into pass1 algo as auxiliary method
    - factored scandata version number into a helper function within main

--- processScribe.py
+++ processScribe.py
@@ -1,10 +1,14 @@
+#-*- coding: utf-8 -*-
 #!/usr/bin/python
 
 """
-This script preprocesses images from an IA Scribe bookscanner.
-For each image found in scandata.xml, it calculates a cropbox
-and skew angle, and writes the results back into scandata.xml.
-These can later be adjusted manually in Republisher.
+    processScribe
+    ~~~~~~~~~~~~~
+
+    This script preprocesses images from an IA Scribe bookscanner.
+    For each image found in scandata.xml, it calculates a cropbox
+    and skew angle, and writes the results back into scandata.xml.
+    These can later be adjusted manually in Republisher.
 """
 
 import argparse
@@ -14,7 +18,6 @@ import re
 import sys
 import pipes
 import math
-#import xml.etree.ElementTree as ET
 from lxml import etree as ET
 
 __version__ = 0.1
@@ -29,7 +32,7 @@ def rmtag(tag, parent):
 
     usage:
     >>> from StringIO import StringIO
-    >>> from lxml.etree import parse
+    >>> from lxml import etree
     >>> xml = "<parent><child>1</child><child>2</child></parent>"
     >>> xmltree = etree.parse(StringIO(xml))
     >>> xmltree.findall("child")
@@ -49,9 +52,6 @@ def addCropBox(tag, parent, x, y, w, h):
     params:
     :param x, y: (x, y) coordinates where to add the cropbox
     :param w, h: width and height to make the cropbox
-
-    usage:
-    >>> # No example provided
     """
     rmtag(tag, parent)
     cropBox = ET.SubElement(parent, tag)
@@ -70,20 +70,64 @@ def crop_score(length, mean, std_dev):
     score = 5.0 - (diff / std_dev)
     return 0.0 if score < 0.0 else score
 
+def mean_var(crops, key1, key2, std_dev=None):
+    """Calculates the mean and variance of two crop types within a dict of crops
+
+    params:
+
+    :param std_dev: a standard deviation. If a value is provided for
+                    stddev other than the value None, mean_var will
+                    return a second mean and variance which exclude
+                    outliers that are more than one std dev from mean.
+    usage: 
+    >>> mean_var(crops, 'CleanCropL', 'CleanCropR')
+    12.3, .89
+    >>> mean_var(crops, 'CleanCropL', 'CleanCropR', std_dev=7)
+    (12.3, 6.21), (.89, .22)
+    """
+    values = [float(crop[key2] - crop[key1] + 1) for i, crop in enumerate(crops)]
+    samplesize = len(values)
+    total = sum(values)
+    mean = total / samplesize
+    variance = sum(((value - mean) ** 2) for value in values) / samplesize
+
+    if std_dev is not None:
+        mean2, variance2 = None, None
+        #return (mean, mean2), (variance, variance2)
+        raise NotImplementedError
+    return mean, variance
+
 def addCropBoxAutoDetect(leaf, c, cropx, cropy, cropw, croph,
                          width_mean_xo, height_mean_xo,
                          width_std_dev_xo, height_std_dev_xo):
-    rmtag('cropBoxAutoDetect', leaf)
-    cropBox = ET.SubElement(leaf, 'cropBoxAutoDetect')    
+    """
+    usage:
+    >>> from StringIO import StringIO
+    >>> from lxml import etree
+    >>> xml = "<parent><child>1</child><child>2</child></parent>"
+    >>> xmltree = etree.parse(StringIO(xml))
+    >>> leaf = xmltree.getroot()
+    >>>
+    """
 
-    crop_width = c['CleanCropR'] - c['CleanCropL'] + 1
-    crop_height = c['CleanCropB'] - c['CleanCropT'] + 1
-    score_width = crop_score(crop_width, width_mean_xo, width_std_dev_xo)
-    score_height = crop_score(crop_height, height_mean_xo, height_std_dev_xo)
-    score = score_width + score_height # score ranges from 0 to 10
-    ET.SubElement(cropBox, 'cropScore').text = "%0.2f" % score
+    def init_cropbox():
+        """Appends a new xml tag called 'cropBoxAutoDetect' to the
+        xmltree, calculates the accumulative crop_score for the crop,
+        c, and appends this value as field 'cropScore' under its
+        parent 'cropBoxAutoDetect'
+        """
+        rmtag('cropBoxAutoDetect', leaf)
+        cropBox = ET.SubElement(leaf, 'cropBoxAutoDetect')
+        crop_width = c['CleanCropR'] - c['CleanCropL'] + 1
+        crop_height = c['CleanCropB'] - c['CleanCropT'] + 1
+        score_width = crop_score(crop_width, width_mean_xo, width_std_dev_xo)
+        score_height = crop_score(crop_height, height_mean_xo, height_std_dev_xo)
+        score = score_width + score_height # score ranges from 0 to 10
+        ET.SubElement(cropBox, 'cropScore').text = "%0.2f" % score
+        return cropBox
 
-    pass2Crop = ET.SubElement(cropBox, 'pass2Crop')
+    cropbox = init_cropbox()
+    pass2Crop = ET.SubElement(cropbox, 'pass2Crop')
     for k, v in zip(('x', 'y', 'w', 'h'), (cropx, cropy, cropw, croph)):
         ET.SubElement(pass2Crop, k).text = str(v)
 
@@ -91,43 +135,17 @@ def addCropBoxAutoDetect(leaf, c, cropx, cropy, cropw, croph,
         # Iterate over borders: top, right, bottom, left
         for border in ('t', 'r', 'b', 'l'): 
             cropkey = '%sCrop%s' % (crop.capitalize(), border.capitalize())
-            croptype = ET.SubElement(cropBox, "%sCrop" % crop)
+            croptype = ET.SubElement(cropbox, "%sCrop" % crop)
             ET.SubElement(croptype, border).text = str(c[cropkey])    
 
-
-# parse_int()
-#_______________________________________________________________________________
 def parse_int(key, output, d):
-    m=re.search(key + ': (-?\d+)', output)
+    m = re.search(key + ': (-?\d+)', output)
     assert(None != m)
     val = int(m.group(1))
     print "%s = %d" % (key, val) 
     d[key] = val
     return val
 
-# calc_mean_var()
-#_______________________________________________________________________________
-def calc_mean_var(crops, keyA, keyB):
-    mean = 0.0;
-    num_leafs = 0;
-    for leafnum,c in crops.items():
-        w = float(c[keyB] - c[keyA] + 1)
-        mean += w
-        num_leafs+=1
-
-    mean /= num_leafs
-    
-    var = 0;
-
-    for leafnum,c in crops.items():
-        w = float(c[keyB] - c[keyA] + 1)
-        var += ( (w-mean)*(w-mean) )
-
-    var /= num_leafs        
-    
-    print 'mean = %f' % mean
-    print 'var = %f' % var
-    return mean, var     
 
 # calc_mean_var_xo()
 # exclude outliers that are more than one std dev from mean, and
@@ -239,21 +257,47 @@ def fit_crop_height_simple(c, mean_height, height_std_dev, img_height):
             
     return cropy, croph, normalize_height
 
+def parse_attrs(output):
+    """Auxiliary method which parses output of:
+    $ ./autoCropScribe imgfp rotateDir
+    """
+    m = re.search('skewMode: (\w+)', output)
+    assert(None != m)
+    skewMode = m.group(1)
+    print "skewMode is " + skewMode
+    
+    m = re.search('angle: ([-.\d]+)', output)
+    assert(None != m)
+    textSkew = float(m.group(1))
+    
+    m = re.search('conf: ([-.\d]+)', output)
+    assert(None != m)
+    textScore = float(m.group(1))
 
-# get_jpg()
-#______________________________________________________________________________
-def get_jpg(id, leafNum, jpg_dir):
-    jpg = pipes.quote('%s/%s_orig_%04d.JPG' % (jpg_dir, id, leafNum))
-    if not os.path.exists(jpg):
-        jpg = pipes.quote('%s/%s_orig_%04d.jpg' % (jpg_dir, id, leafNum))
+    m = re.search('bindingAngle: ([-.\d]+)', output)
+    assert(None != m)
+    bindingSkew = float(m.group(1))
+    
+    m = re.search('grayMode: ([\w-]+)', output)
+    grayMode = m.group(1)
+    print "grayMode is " + grayMode
+    
+    return skeyMode, textSkew, textScore, bindingSkey, grayMode
+
+
+def auto_crop_pass1(bid, leafs, jpg_dir):
+    """
+    """
+    def get_jpg(path_id, leafNum, jpg_dir):
+        """Auxiliary method to resolve path of jpg"""
+        _path = '%s/%s_orig_%04d' % (jpg_dir, _id, leafNum)
+        jpg = pipes.quote('%s.JPG' % _path) 
+        if not os.path.exists(jpg):
+            jpg = pipes.quote('%s.jpg' % _path)
         
-    assert os.path.exists(jpg)
-    
-    return jpg
-    
-# auto_crop_pass1()
-#______________________________________________________________________________
-def auto_crop_pass1(id, leafs, jpg_dir):
+        assert os.path.exists(jpg)    
+        return jpg
+
     crops = {}
 
     for leaf in leafs:
@@ -266,58 +310,28 @@ def auto_crop_pass1(id, leafs, jpg_dir):
             continue #skip first deleted page
 
         rotateDegree = int(leaf.findtext('rotateDegree'))
-        assert((-90 == rotateDegree) or (90 == rotateDegree))    
-        if (90 == rotateDegree):
-            rotateDir = 1
-        elif (-90 == rotateDegree):
-            rotateDir = -1
-
+        assert(abs(rotateDegree) == 90)
+        rotateDir = rotateDegree / 90
         print "rotateDir = %d" % rotateDir
-        
-        file = get_jpg(id, leafNum, jpg_dir)
-        cmd = "./autoCropScribe %s %d" % (file, rotateDir)
+
+        # XXX - C bindings should be written rather than cmd
+        impfp = get_jpg(bid, leafNum, jpg_dir)
+        cmd = "./autoCropScribe %s %d" % (imgfp, rotateDir)
         print cmd
-        retval,output = commands.getstatusoutput(cmd)
+        retval, output = commands.getstatusoutput(cmd)
         if 0 != retval:
             print "retval is %d" % (retval)
             print output
             
-        assert (0 == retval)        
+        assert (0 == retval)
+    
+        skeyMode, textSkew, textScore, bindingSkey, grayMode = parse_attrs()
 
-
-        m=re.search('skewMode: (\w+)', output)
-        skewMode = m.group(1)
-        print "skewMode is " + skewMode
-        
-        m=re.search('angle: ([-.\d]+)', output)
-        assert(None != m)
-        textSkew = float(m.group(1))
-    
-        m=re.search('conf: ([-.\d]+)', output)
-        assert(None != m)
-        textScore = float(m.group(1))
-        
-        m=re.search('bindingAngle: ([-.\d]+)', output)
-        assert(None != m)
-        bindingSkew = float(m.group(1))
-    
-        m=re.search('grayMode: ([\w-]+)', output)
-        grayMode = m.group(1)
-        print "grayMode is " + grayMode
-    
         crops[leafNum] = {}
-        parse_int('OuterCropL', output, crops[leafNum] )
-        parse_int('OuterCropR', output, crops[leafNum] )
-        parse_int('OuterCropT', output, crops[leafNum] )
-        parse_int('OuterCropB', output, crops[leafNum] )
-        parse_int('CleanCropL', output, crops[leafNum] )
-        parse_int('CleanCropR', output, crops[leafNum] )
-        parse_int('CleanCropT', output, crops[leafNum] )
-        parse_int('CleanCropB', output, crops[leafNum] )
-        parse_int('InnerCropL', output, crops[leafNum] )
-        parse_int('InnerCropR', output, crops[leafNum] )
-        parse_int('InnerCropT', output, crops[leafNum] )
-        parse_int('InnerCropB', output, crops[leafNum] )
+        for croptype in ['Outer', 'Clean', 'Inner']:
+            for border in ['L', 'R', 'T', 'B']:
+                parse_int('%sCrop%s' % (croptype, border), output, crops[leafNum])
+
         crops[leafNum]['angle'] = textSkew
         crops[leafNum]['angleConf'] = textScore
         crops[leafNum]['skewMode'] = skewMode
@@ -332,8 +346,8 @@ def auto_crop_pass1(id, leafs, jpg_dir):
 # auto_crop_pass2()
 #______________________________________________________________________________
 def auto_crop_pass2(leafs, crops):
-    width_mean_full, width_var_full     = calc_mean_var(crops, 'CleanCropL', 'CleanCropR')
-    height_mean_full, height_var_full   = calc_mean_var(crops, 'CleanCropT', 'CleanCropB')
+    width_mean_full, width_var_full     = mean_var(crops, 'CleanCropL', 'CleanCropR')
+    height_mean_full, height_var_full   = mean_var(crops, 'CleanCropT', 'CleanCropB')
     width_std_dev_full                  = math.sqrt(width_var_full)
     height_std_dev_full                 = math.sqrt(height_var_full)
     
@@ -409,19 +423,23 @@ def parsexml(xmlfp):
     return xmltree
 
 if __name__ == "__main__":
+    def set_autocrop_version(scandata):
+        """Injects the correct version number into the xmltree under
+        the 'autoCropVersion' field
+        """
+        bookdata = scandata.find(BOOK_DATA)
+        rmtag('autoCropVersion', bookdata)
+        ET.SubElement(bookdata, 'autoCropVersion').text = str(__version__)
+
     parser = argparser()
     args = parser.parse_args()
     xmltree = parsexml(args.xml)
 
     scandata = xmltree.getroot()
-    bookdata = scandata.find(BOOK_DATA)
     bid = scandata.findtext(BOOK_ID)
-    pagess = scandata.findall(PAGES)
-    
-    print 'Autocropping ' + bid
-    rmtag('autoCropVersion', bookdata)
-    ET.SubElement(bookdata, 'autoCropVersion').text = str(__version__)
-    
+    pages = scandata.findall(PAGES)
+    set_autocrop_version(scandata)
+
     crops = auto_crop_pass1(bid, pages, args.imgdir)
     auto_crop_pass2(pages, crops)
     
